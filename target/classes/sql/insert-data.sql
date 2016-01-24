2b27f225adf1455e17c4528bd877ac0bd9475502
Velius5 <patryk.dobrzynski@o2.pl>
2016-01-24 16:46:24 +0100

    Prod Update

--- target/classes/sql/insert-data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO uzytkownicy (imie,nazwisko,email,haslo,aktywny,uprawnienia) VALUES ('Patryk','Dobrzyński','kontakt@patrykdobrzynski.eu','e10adc3949ba59abbe56e057f20f883e','1','1');
-INSERT INTO uzytkownicy (imie,nazwisko,email,haslo,aktywny,uprawnienia) VALUES ('Zenon','Kowalski','zenek@wp.pl','e10adc3949ba59abbe56e057f20f883e','1','2');
-INSERT INTO uzytkownicy (imie,nazwisko,email,haslo,aktywny,uprawnienia) VALUES ('Eugeniusz','Nowak','eugeniusz@o2.pl','e10adc3949ba59abbe56e057f20f883e','1','2');
-INSERT INTO uzytkownicy (imie,nazwisko,email,haslo,aktywny,uprawnienia) VALUES ('Robert','Jakiśtam','robert.xxx@wp.pl','e10adc3949ba59abbe56e057f20f883e','1','2');
