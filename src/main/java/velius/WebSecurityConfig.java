d3e6c28a782d4cafd44e8e7da63ad6b93cdab7ee
Velius5 <patryk.dobrzynski@o2.pl>
2016-01-28 17:25:14 +0100

    Logowanie z szyfrowaniem hasła

--- src/main/java/velius/WebSecurityConfig.java
+++ src/main/java/velius/WebSecurityConfig.java
@@ -7,6 +7,7 @@ package velius;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -40,7 +41,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth
-            .userDetailsService(userDS);
+            .userDetailsService(userDS)
+                .passwordEncoder(new Md5PasswordEncoder());
     }
     
     @Override
