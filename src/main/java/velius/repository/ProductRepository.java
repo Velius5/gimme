2c2c729c230a6580e666c898270d9f91570d36dd
Velius5 <patryk.dobrzynski@o2.pl>
2016-04-17 04:05:48 +0200

    Usunięcie konfliktów

--- src/main/java/velius/repository/ProductRepository.java
+++ src/main/java/velius/repository/ProductRepository.java
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-/*
- * Author: Patryk Dobrzyński
- * Author URL: http://patrykdobrzynski.eu
- * Author Email: kontakt@patrykdobrzynski.eu
- */
-package velius.repository;
-
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-import velius.model.Product;
-import velius.model.User;
-
-/**
- *
- * @author Patryk
- */
-public interface ProductRepository extends JpaRepository<Product, String> {
-    List<Product> findAllByOwner(User user);
-    List<Product> findAllByUsers(User user);
-}
-=======
 /*
  * Author: Patryk Dobrzyński
  * Author URL: http://patrykdobrzynski.eu
@@ -40,4 +18,3 @@ public interface ProductRepository extends JpaRepository<Product, String> {
     List<Product> findIdDistinctByOwnerAndUsersNotNull(User user);
     List<Product> findAllByUsers(User user);
 }
->>>>>>> cbf69cf021552409f2e3d375a47d9da42d236b43
