ae7884d0245ef4261d36abd10c9f13b7a815eea3
Piotr Czarny <piotr0116@mat.umk.pl>
2016-04-29 00:39:17 +0200

    Friend Activity
    Poprawione wyswietlanie listy znajomych

--- Android/Aplikacja/app/src/main/java/zespolowe/pl/aplikacja/Friend_Activity.java
+++ Android/Aplikacja/app/src/main/java/zespolowe/pl/aplikacja/Friend_Activity.java
@@ -1,12 +1,15 @@
 package zespolowe.pl.aplikacja;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,6 +20,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import zespolowe.pl.aplikacja.functions.SessionManager;
 import zespolowe.pl.aplikacja.model.Friend;
+import zespolowe.pl.aplikacja.model.Product;
 import zespolowe.pl.aplikacja.model.User;
 import zespolowe.pl.aplikacja.services.UserService;
 
@@ -34,82 +38,75 @@ public class Friend_Activity extends Activity {
     List<Friend> friends = new ArrayList<>();
     SessionManager session;
     User user;
+    Activity act;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         session = new SessionManager(getApplicationContext());
         user = session.getUserDetails();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.friend_main);
         System.out.println("Friend_Activity");
+        System.out.println("postecexute");
+        act = this;
 
+        if (friends.isEmpty())
+            new AsyncTask<Void, Void, Void>() {
 
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(SessionManager.getAPIURL())
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
+                @Override
+                protected Void doInBackground(Void... params) {
+                    System.out.println("Start");
+                    Retrofit retrofit = new Retrofit.Builder()
+                            .baseUrl(SessionManager.getAPIURL())
+                            .addConverterFactory(GsonConverterFactory.create())
+                            .build();
+                    UserService userService = retrofit.create(UserService.class);
+                    Call<List<Friend>> call = userService.getFriendList(user.getId());
 
-        UserService userService = retrofit.create(UserService.class);
-        Call<List<Friend>> call = userService.getFriendList(user.getId());
-        call.enqueue(new Callback<List<Friend>>() {
-            @Override
-            public void onResponse(Call<List<Friend>> call, Response<List<Friend>> response) {
-                List<Friend> resp = response.body();
-                //friends = resp;
-                for (Friend friend : resp) {
-                    friends.add(new Friend(friend.getId(), friend.getName(), friend.getSurname()/*, friend.getBilans()*/));
-                    System.out.println(friend.toString());
+                    Response<List<Friend>> friend = null;
+                    try {
+                        friend = call.execute();
+                        System.out.println("Pobrano znajomych");
+                    } catch (Exception e) {
+                        System.out.println("Błąd pobierania danych");
+                    }
+                    friends = friend.body();
+
+                    return null;
                 }
-                //onAddSuccess();
-            }
 
-            @Override
-            public void onFailure(Call<List<Friend>> call, Throwable t) {
-                System.out.println("Blad.");
-                //onAddFailed("Zmiana nie powiodła się");
-            }
-        });
+                @Override
+                protected void onPostExecute(Void result) {
+                    super.onPostExecute(result);
 
+                    list = (ListView) findViewById(R.id.list);
+                    CustomListAdapter adapter = new CustomListAdapter(act, friends);
+                    list.setAdapter(adapter);
 
+                    list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
+                        @Override
+                        public void onItemClick(AdapterView<?> parent, View view,
+                                                int position, long id) {
+                            // TODO Auto-generated method stub
+                            Friend Slecteditem = friends.get(position);
+                            Intent intent = new Intent(/*getApplicationContext()*/ Friend_Activity.this, Splac_Activity.class);
+                            intent.putExtra("saldo", Slecteditem.getBilans());
+                            intent.putExtra("name", Slecteditem.getName());
+                            intent.putExtra("surname", Slecteditem.getSurname());
+                            intent.putExtra("id", Slecteditem.getId());
+                            System.out.println(Slecteditem.getId());
+                            startActivity(intent);
+                            //  Intent intent = new Intent(this, User_Profile_Activity.class)
+                            //    startActivity(intent);
+                            // Toast.makeText(getApplicationContext(), (CharSequence) Slecteditem, Toast.LENGTH_SHORT).show();
 
-        CustomListAdapter adapter=new CustomListAdapter(this, friends);
-        list=(ListView)findViewById(R.id.list);
-        list.setAdapter(adapter);
+                        }
 
-        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view,
-                                    int position, long id) {
-                // TODO Auto-generated method stub
-                Friend Slecteditem = friends.get(position);
-                Intent intent = new Intent(/*getApplicationContext()*/ Friend_Activity.this, Splac_Activity.class);
-                intent.putExtra("id", Slecteditem.getId());
-                System.out.println(Slecteditem.getId());
-                startActivity(intent);
-
-
-
-            }
-
-        });
+                    });
+                    System.out.println("koniec");
+                }
+            }.execute();
     }
-
-//    class MyAsyncTask extends AsyncTask<String, String, String>{
-//
-//        @Override
-//        protected String doInBackground(String... params) {
-//            return null;
-//        }
-//        @Override
-//        protected void onPreExecute(){
-//            super.onPreExecute();
-//        }
-//
-//        @Override
-//        protected void onPostExecute(String result) {
-//            super.onPostExecute(result);
-//        }
-//    }
 }
